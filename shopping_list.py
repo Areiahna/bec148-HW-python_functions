dae029700b6261decbea84d69f269b4a8a95dfcd
areiahna <areiahnacooks@yahoo.com>
2024-06-05 22:33:25 -0400

    Updated code

--- shopping_list.py
+++ shopping_list.py
@@ -1,34 +1,87 @@
 # Task 1: Write a function that lets the user add items to a list, make sure you ask the user what they would like to add (reminder: ensure the function has a parameter to receive the list). 
 shopping_list = []    
 
-def add_to_list (list):
+def main(list):
     while True:
-        item = input("What would you like to add to your shopping list?: ")
-        shopping_list.append(item)
-        add_item = input("Would you like to add another (y/n): ")
-        if (add_item == "y"):
-            pass
-        elif (add_item !="y"):
-            # Task 2: Include a feature to remove items from the list.
-            print("="*50)
-            remove_item = input("Would you like to remove an item from your list? (y/n): ")
-            if (remove_item == "y"):
-                print(shopping_list)
-                remove = input("Which item would you like to remove?: ")
-                for item in shopping_list:
-                    if (remove == item):
-                        shopping_list.remove(item)
-                    else:
-                        print("="*50) 
+        ans = input('''
+        Shopping list Maker
+        What would you like to do?
+        
+        1. Make a list
+        2. Remove an item from your list
+        3. View Shopping list
+        4. Quit
+
+        ''')
+        
+        if (ans == "1"):
+           add_item()
+        elif (ans == '2'):
+            delete_item()
+
+        elif(ans == '3'):
+            view_list()
+
+        elif(ans == '4'):
+            print(" Thanks for using my app!")
+            break
+
+def add_item():
+    while True:
+        ans = input("What would you like to add?: ")
+        shopping_list.append(ans)
+        ans = input('''
+        Would you like to add another item?
+        y - yes
+        n - no
+        ''')
+        if ans == "y":
+            continue
+        else:
             break
 
         
-add_to_list(shopping_list)
 
-# Task 3: Add a function that prints out the entire list.
-def view_list (list):
-    print("Your shopping list:")
-    for item in range(len(list)):
-       print(f"- {list[item]}")
-       
-view_list(shopping_list)
+def delete_item():
+    item = input("Which item would you like to remove?: ")
+    while True:
+        try:
+            shopping_list.remove(item)
+        except ValueError:
+            print(f"It looks like you don't have {item} on your list\n")
+            break
+        else:
+            print(f"Successfully removed {item} from your list!\n")
+            break
+        finally:
+            view_list()
+
+def view_list ():
+    print("Your current shopping list:")
+    print("~"*25)
+    for item in range(len(shopping_list)):
+       print(f"- {shopping_list[item]}")
+
+        
+main(shopping_list)
+
+  # elif ans 
+        #     # Task 2: Include a feature to remove items from the list.
+        #     print("="*50)
+        #     remove_item = input("Would you like to remove an item from your list? (y/n): ")
+        #     if (remove_item == "y"):
+        #         print(shopping_list)
+        #         remove = input("Which item would you like to remove?: ")
+        #         for item in shopping_list:
+        #             if (remove == item):
+        #                 shopping_list.remove(item)
+        #             else:
+        #                 print("="*50) 
+        #     break
+
+    #   for item in shopping_list:
+    #     if (ans == item):
+    #         shopping_list.remove(item)
+    #     else: 
+    #         print("Sorry that's not on the list")
+    #         break
